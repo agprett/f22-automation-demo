3ef0a52e54cc54892933e29c5b5455ab85ab20a5
Alec Prettyman <prettale4@gmail.com>
2022-05-17 13:36:14 -0600

    finished demo

---
+++ movie-list/__tests__/movie-list.test.js
@@ -0,0 +1,21 @@
+const {Builder, Capabilities} = require('selenium-webdriver')
+
+require('chromedriver')
+
+const {addMovie} = require('../functions')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeEach(async () => {
+  await driver.get('http://127.0.0.1:5500/movie-list/index.html')
+})
+
+afterAll(async () => {
+  await driver.quit()
+})
+
+test('Add a movie', async () => {
+  await addMovie(driver)
+
+  await driver.sleep(3000)
+})
